8c57a0bc56917b74dd416afcc98c71523c976085
github username <github email address>
2019-01-26 18:25:37 +0000

    learn submit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here:
+
+const driversWithRevenueOver = (driver, revenue) => {
+  return driver.filter(obj => obj.revenue > revenue)
+}
+
+function driverNamesWithRevenueOver (driver, revenue) {
+  return driversWithRevenueOver(driver, revenue).map(obj => obj.name)
+}
+
+const exactMatch = function (driver, att) {
+  let key1 = Object.keys(att)[0]
+  let newArr = [];
+//   debugger; 
+  newArr = driver.filter(function (obj) {
+    if (obj[key1]) {
+        
+      return obj[key1] === att[key1]
+    }
+  })
+//   debugger; 
+  return newArr
+  
+}
+
+function exactMatchToList(driver, att) {
+    return exactMatch(driver, att).map( obj => obj.name)
+}
