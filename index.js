34418f5b6f8b1600d6446c6388e27d69ddccdf44
Dark-Raine <anderson-tomas@hotmail.com>
2019-01-30 15:13:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here:
+let driversWithRevenueOver = (drivers, amt) =>
+    drivers.filter(driver => driver.revenue > amt)
+
+let driverNamesWithRevenueOver = (drivers, amt) =>
+    driversWithRevenueOver(drivers, amt).map(driver => driver.name)
+
+let exactMatch = (drivers, obj) =>
+    drivers.filter(driver => driver[Object.keys(obj)[0]] === obj[Object.keys(obj)[0]])
+
+let exactMatchToList = (drivers, obj) =>
+    exactMatch(drivers, obj).map(driver => driver.name)
