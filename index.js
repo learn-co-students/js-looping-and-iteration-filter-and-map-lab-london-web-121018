b7a76665686d7bb267dcc03514014f23d53aea77
github username <github email address>
2019-01-30 15:14:55 +0000

    Done.

--- index.js
+++ index.js
@@ -15,19 +15,21 @@ function driverNamesWithRevenueOver(drivers, revenue){
 }
 
 function exactMatch(array, value){
-    return array.filter(driver => driver.name == value.name || driver.revenue === value.revenue);
+    return array.filter(driver => driver.name === value.name || driver.revenue === value.revenue);
 }
 
 function exactMatchToList(extendedDrivers, hash){
-    let matchList = [];
-    for (const key in extendedDrivers){
-        if (typeof extendedDrivers[key] === 'object') {
-          for (const nestedKey in extendedDrivers[key]) {
-            if (extendedDrivers[key][nestedKey] === Object.values(hash)[0]){
-                matchList.push(extendedDrivers[key].name);
-            }
-          }
-        }
-    }
-    return matchList;
+    // let matchList = [];
+    // for (const key in extendedDrivers){
+    //     if (typeof extendedDrivers[key] === 'object') {
+    //       for (const nestedKey in extendedDrivers[key]) {
+    //         if (extendedDrivers[key][nestedKey] === Object.values(hash)[0]){
+    //             matchList.push(extendedDrivers[key].name);
+    //         }
+    //       }
+    //     }
+    // }
+    // return matchList;
+
+    return exactMatch(extendedDrivers, hash).map( driver => driver.name);
 }
